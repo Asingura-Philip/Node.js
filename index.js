388b9ba99be3e135c173e56846fc6b14b38d0d97
Asingura Philip <asinguraphilip@gmail.com>
2024-08-02 10:04:40 +0300

    anotomy express

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+//dependencies
 const express = require("express");
+
+//instantiations
 const app = express();
 
+//middleware
 app.use(express.urlencoded({ extended: true }));
+// Simple request time logger
+app.use((req, res, next) => {
+  console.log('A new request received at ' + Date.now());
+  next();
+});
 
+//Simple request time logger for a specific route
+app.use('/about', (req, res, next) => {
+  console.log('A new request received at ' + Date.now());
+  next();
+  });
+  
+
+
+//routes
 app.get("/", (req, res) => {
   // new
   res.send("Homepage! Hello world.");
@@ -43,4 +61,7 @@ app.get("*", (req, res) => {
   res.send("Error!!!!. page not found");
 });
 
+
+
+//bootstrapping server
 app.listen(3000, () => console.log("listening on port 3000"));
