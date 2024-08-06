96621fb1a9945de64715edb21b08206149ae69c6
Asingura Philip <asinguraphilip@gmail.com>
2024-08-06 21:34:01 +0300

    pug routes

--- routes/studyRoutes.js
+++ routes/studyRoutes.js
@@ -67,4 +67,25 @@ router.post("/first", (req, res) => {
   console.log(req.body);
 });
 
+//page 1
+router.get("/page1", (req, res) => {
+  res.render("page1");
+});
+
+
+//page 2
+router.get("/page2", (req, res) => {
+  res.render("page2");
+});
+
+router.post("/page2", (req, res) => {
+  console.log(req.body);
+});
+//page 3
+router.get("/page3", (req, res) => {
+  res.render("index");
+});
+
+
+
 module.exports = router;
